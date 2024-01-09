70e39bbc8772756f7259b0da9f77c2466c48c3e9
edobbin <ethandobbin37@gmail.com>
2024-01-09 11:13:26 -0500

    navbar

--- edobbi/src/components/Navbar/navbar.js
+++ edobbi/src/components/Navbar/navbar.js
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { navData } from "./navData"
 import './navbar.css'
 
-const navbar = (props) => {
+
+const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
+  const toggle = () =>{
+    setNavOpen(!navOpen)
+  }
+
+
   return (
     <div className='navbar'>
     <div className='Title-container'>
       {/* <img></img> */}
       
     </div>
-    <div className='sidebar'>
-      <a href='#section'>About</a>
+    
+    {/* <nav className={'nav ${navOpen ? "nav-open" : "nav-close"}'}> */}
+    <div className={navOpen ? "sidebar": "sidebarClosed"}>
+    <button className='nav-Tog' onClick={() => toggle()}
+    >
+      <span className="material-symbols-outlined" Style={navOpen? "font-size: 40px; align-items: center;":"font-size: 36px; align-items: center; "}>
+{navOpen? "menu_open":"menu"}
+</span>
+    </button>
+    {navData.map(item =>{
+      return <div key={item.id}>
+        
+        <a href='#section'>{navOpen ? item.text : item.icon}</a>
+      </div>   
+    })}
+      {/* <a href='#section'>About</a>
       <a href='#section'>Projects</a>
       <a href='#section'>Resume</a>
-      <a href='#section'>Blog</a>
+      <a href='#section'>Blog</a> */}
     </div>
+    {/* </nav> */}
       </div>
   );
 };
 
-export default navbar;
+export default Navbar;
