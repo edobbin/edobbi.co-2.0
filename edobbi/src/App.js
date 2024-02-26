57bb7d15758618227c1bf819d3516dd4b867971e
edobbin <ethandobbin37@gmail.com>
2024-02-26 09:30:40 -0500

    New top nav bar

--- edobbi/src/App.js
+++ edobbi/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
 
-    <Nav name = 'dob'/> 
+    {/* <Nav name = 'dob'/>  */}
     <div className='container'>
     <Home/>
     <About />
---
+++ edobbi/src/components/Navigation/navData.js
@@ -0,0 +1,37 @@
+import HomeIcon from '@mui/icons-material/Home';
+import About from '@mui/icons-material/Info';
+import Project from '@mui/icons-material/DataObject';
+import Blog from '@mui/icons-material/KeyboardAlt';
+import Resume from '@mui/icons-material/Article';
+ 
+export const navData = [
+    {
+        id: 0,
+        icon: <HomeIcon/>,
+        text: "Home",
+        link: "/"
+    },
+    {
+        id: 1,
+        icon: <About/>,
+        text: "About",
+        link: ""
+    },
+    {
+        id: 2,
+        icon: <Project/>,
+        text: "Projects",
+        link: ""
+    },
+    {
+        id: 3,
+        icon: <Blog/>,
+        text: "Blog",
+        link: ""
+    },{
+        id: 4,
+        icon: <Resume/>,
+        text: "Resume",
+        link: ""
+    }
+]
