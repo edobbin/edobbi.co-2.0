4d2a2a3c7fa4e66876777c6602bb64ec58c0a2af
edobb <ethandobbin37@gmail.com>
2025-08-28 20:49:49 -0400

    2025 Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './master.css'
 function App() {
   return (
     <div className="App">
-
+    <Nav />
     {/* <Nav name = 'dob'/>  */}
     <div className='container'>
     <Background />
--- src/components/sections/about/about.js
+++ src/components/sections/about/about.js
@@ -10,41 +10,9 @@ const about = () => {
             <p className='AM'>Howdy, my name is Ethan and I am a Student at Towson University majoring in Computer Science. Throughout my college journey I've built sa strong foundation in HTML, CSS, JavaScript, Java and Python. Along with experience using React.JS, and Node.JS. I love making projects and learning new skills whether it be in my own freetime or through class projects. I began my programming in high school wher I was able to learn java. </p>
           
           </div>
-
+      <div>
       <SkillContainer />
-      {/* <div className='skills'>
-
-        <div className='languages'>
-            <h4 className='skill_title'>Lanuages</h4>
-          <ul>
-            <li>Java</li>
-            <li>JavaScript</li>
-            <li>SQL</li>
-            <li>HTML</li>
-            <li>CSS</li>
-          </ul>
-        </div>
-
-        <div className='LanF'>
-          <h4 className='skill_title'>Libraries and Frameworks</h4>
-          <ul>
-            <li>React.js</li>
-            <li>Node.js</li>
-          </ul>
-        </div>
-
-        <div className='Tools'>
-          <h4 className='skill_title'>Tools</h4>
-          <ul>
-            <li>Firebase FireStore Database</li>
-            <li>Firebase Realtime Database</li>
-            <li>AWS EC2</li>
-            <li>Github</li>
-            <li>Git</li>
-          </ul>
-        </div>
-
-      </div> */}
+      </div>
         </div>
     );
 };
