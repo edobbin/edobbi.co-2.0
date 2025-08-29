698aa5354b134d73fec2c7c5abba739a59355345
edobb <ethandobbin37@gmail.com>
2025-08-29 14:15:56 -0400

    Experience Timeline

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from './components/Navbar/navbar.js'
 import Home from './components/sections/home/home.js'
 import About from './components/sections/about/about.js'
 import Background from './components/background/background.jsx';
-import { Experience } from './components/Experience/Experience.js';
+import { Experience } from './components/sections/Experience/Experience.js';
 import './master.css'
 
 function App() {
---
+++ src/components/sections/Experience/Experience.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import data from './Experiences.json'
+import { IoSchool } from "react-icons/io5";
+import { MdOutlineWork } from "react-icons/md";
+import './Experience.css'
+export const Experience = () => {
+  return (
+    <div className='containerE'>
+      
+        <VerticalTimeline 
+        animate= {true}
+        lineColor='white'>
+          
+          {data.sort((a,b)=> b.id-a.id).map(item=> {
+            if(item.type==="education"){
+              return(
+              <VerticalTimelineElement
+              key={item.id}
+              className="custom-timeline-element"
+              icon ={<IoSchool size={20} />}
+              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff'}}
+              date={`${item.startDate} - ${item.endDate}`}>
+                <h3 className="vertical-timeline-element-title">{item.company}</h3>
+                <h4 className="vertical-timeline-element-subtitle">{item.title}</h4>
+                
+              </VerticalTimelineElement>
+          );
+            }
+
+            else if(item.type==="work"){
+              return(
+              <VerticalTimelineElement
+              key={item.id}
+              icon ={<MdOutlineWork size={20} />}
+              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+              date={`${item.startDate} - ${item.endDate}`}
+              dateClassName={ "dateClass" }>
+                <h3 className="vertical-timeline-element-title">{item.company}</h3>
+                <h4 className="vertical-timeline-element-subtitle">{item.title}</h4>
+                <br />
+                <h5 className="vertical-timeline-element-summary">{item.summary}</h5>
+              </VerticalTimelineElement>
+              );
+            }
+          }
+
+          )}    
+        </VerticalTimeline>
+
+    </div>
+  )
+}
