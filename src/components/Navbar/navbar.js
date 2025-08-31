125fc1eeb0ba2eecb8d55831e34c20c5727b4380
edobb <ethandobbin37@gmail.com>
2025-08-30 21:00:49 -0400

    Mobile navbar 1.1: Removal of unneeded files

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -15,28 +15,22 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <nav>
-    <div className='Title-container'>
-      {/* <img></img> */}
-      
+    <div className='Title-container'>   
     </div>
-    
-    {/* <nav className={'nav ${navOpen ? "nav-open" : "nav-close"}'}> */}
     <div className={navOpen ? "sidebar": "sidebarClosed"}>
-    <button className='nav-Tog' onClick={() => toggle()}
-    >
-      <span className="material-symbols-outlined" Style={navOpen? "font-size: 40px; align-items: center;":"font-size: 36px; align-items: center; "}>
-{navOpen? "menu_open":"menu"}
+    <button className='nav-Tog' onClick={() => toggle()}>
+    <span className="material-symbols-outlined" Style={navOpen? "font-size: 40px; align-items: center;":"font-size: 36px; align-items: center; "}>
+    {navOpen? "menu_open":"menu"}
 </span>
     </button>
     {navData.map(item =>{
       return <div key={item.id}>
-        
         <a href={item.link}>{navOpen ? item.text : item.icon}</a>
       </div>   
     })}
     </div>
-    {/* </nav> */}</nav>
-      </div>
+    </nav>
+    </div>
   );
 };
 
